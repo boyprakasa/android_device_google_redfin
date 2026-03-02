4b6693417ba7247c58124e23b2afa6ab5c06557c
Boy Prakasa <boyprakasa@gmail.com>
2026-03-02 12:37:31 +0700

    bring up

--- derpfest_redfin.mk
+++ derpfest_redfin.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/derpfest/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/redfin/aosp_redfin.mk)
@@ -16,10 +16,10 @@ include device/google/redfin/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 5
-PRODUCT_NAME := infinity_redfin
+PRODUCT_NAME := derpfest_redfin
 
 # Maintainer
-INFINITY_MAINTAINER := Boy Prakasa
+DERPFEST_MAINTAINER := Boy Prakasa
 
 # Whether Including Google Apps
 WITH_GAPPS := true
